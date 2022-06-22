c38df279fcb53e203a278076a4e57413ba5eac83
Joao Rubens Belluzzo Neto <belluzzojr@outlook.com>
2022-06-21 22:10:18 -0300

    JS - Logic Programming

--- module1/aula2/examples/secondFunction.js
+++ module1/aula2/examples/secondFunction.js
@@ -1,4 +1,7 @@
 function showVariable(){
     alert("Returning from break")
     username = "Bellu";
+    age = 20;
+
+    alert("The name in username is " + username + " and age is " + age);
 }
---
+++ module1/aula2/exercises/firstFunction.js
@@ -0,0 +1,7 @@
+function showInfo(){
+
+    username = "Bellu";
+    age = 20;
+    alert("Information in user is...");
+    alert("Username is " + username + " and age is " + age);
+}
