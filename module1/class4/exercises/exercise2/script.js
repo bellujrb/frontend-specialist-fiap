4d80b2d65784f580d615b0e7eba91974b4870549
belluzzojr <98968823+belluzzojr@users.noreply.github.com>
2022-06-29 21:22:30 -0300

    JS - Logic Programming

--- module1/class4/exercises/exercise2/script.js
+++ module1/class4/exercises/exercise2/script.js
@@ -1,3 +1,14 @@
 function dev(){
     
+    quantity = parseInt(prompt("Enter the number of numbers"));
+
+    for(i=0; i<quantity; i++){
+        value = parseInt(prompt("Enter the value"));
+        value += value;
+    }
+    media = value / quantity;
+
+    alert(value);
+    alert(quantity);
+
 }
