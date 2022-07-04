04577a101685476c487b1b66c1a0d604ba3eeba3
belluzzojr <98968823+belluzzojr@users.noreply.github.com>
2022-07-04 20:28:57 -0300

    JS - Logic Programming

--- module1/class4/exercises/exercise7/script.js
+++ module1/class4/exercises/exercise7/script.js
@@ -4,11 +4,11 @@ function dev(){
 
     if (agePlayer >= 18){
         alert("Player PRO")
-    } else if (agePlayer >= 15){
+    } else if ((agePlayer >= 15) && (agePlayer < 18)){
         alert("Player SEMIPRO")
-    } else if (agePlayer >= 12){
+    } else if ((agePlayer >= 12) && (agePlayer < 15)){
         alert("Player ASPIRANT")
-    } else if (agePlayer < 12){
+    } else {
         alert("Baby Tooth")
     }
 }
