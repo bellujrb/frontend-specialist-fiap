d5d5670ee8f9b65e8aaecb1488fb72dee1b29114
belluzzojr <98968823+belluzzojr@users.noreply.github.com>
2022-07-06 22:18:55 -0300

    JS - Logic Programming

---
+++ module1/class6/exercises/exercise1/script.js
@@ -0,0 +1,19 @@
+function dev(){
+
+    alert("Array");
+
+    city = ["Ubatuba", "Caraguatatuba", "Guaruja"];
+    sizeVetor = city.length;
+    age = [1, 2, 3, 4];
+
+    course = [];
+    course[0] = "Analist in System";
+    course[1] = "Computer Science";
+    course[2] = "Software Enginner";
+
+    vetorDefiny = new Array(4);
+
+    alert(city);
+    alert(age);
+    alert(course);
+}
---
+++ module1/class6/exercises/exercise2/script.js
@@ -0,0 +1,8 @@
+function dev(){
+    var alfabet = [
+    ['A', 'B', 'C'],
+    ['D', 'E', 'F'],
+    ['G', 'H', 'I']
+    ['J', 'K', 'L']
+    ];
+}
