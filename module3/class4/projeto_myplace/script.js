a49736ba1ec42cec8961dee438b182cd560e8b3c
belluzzojr <98968823+belluzzojr@users.noreply.github.com>
2022-08-11 14:56:45 -0300

    Frontend Programming

--- module3/class4/projeto_myplace/script.js
+++ module3/class4/projeto_myplace/script.js
@@ -1,6 +1,9 @@
-const menuButton = document.querySelector('.iconlist')
-const navMenu = document.querySelector('.nav-menu')
+const navMenu = document.querySelector('.nav-menu');
+document.addEventListener('click', function(e){
+    if(e.target.classList.contains('iconlist') || e.target.classList.contains('nav-menu')){
+        navMenu.classList.toggle('mobile-menu');
+    } else {
+        navMenu.classList.toggle('mobile-menu');
+    }
 
-menuButton.addEventListener('click', () =>{
-    navMenu.classList.toggle('mobile-menu')
 });
