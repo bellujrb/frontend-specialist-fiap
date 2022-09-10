590adb7995c68ec2cffa56371a5919ddcad8718e
belluzzojr <98968823+belluzzojr@users.noreply.github.com>
2022-09-10 18:47:09 -0300

    React Framework

---
+++ module5/fiap-shift-exercise03/src/Screen/Homes/HomeController.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class HomeController extends Component {
+    constructor(props){
+        super(props);
+    }
+    
+    render() {
+        return (
+            <HomeView/>
+        );
+    }
+}
+
+export default HomeController;
---
+++ module5/fiap-shift-exercise03/src/Screen/Homes/HomeView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+const HomeView = ({ count }) => {
+
+    return (
+        <div>
+            <p>CONTADOR {count}</p>
+        </div>
+    );
+};
+
+export default HomeView;
