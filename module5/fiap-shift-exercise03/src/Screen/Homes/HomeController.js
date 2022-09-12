ad60a7cc90f6d3a27771cfda6a1d9904e439346f
belluzzojr <98968823+belluzzojr@users.noreply.github.com>
2022-09-12 17:28:37 -0300

    React Framework

--- module5/fiap-shift-exercise03/src/Screen/Homes/HomeController.js
+++ module5/fiap-shift-exercise03/src/Screen/Homes/HomeController.js
@@ -1,13 +1,66 @@
 import React, { Component } from 'react';
+import HomeView from './HomeView';
+import HomeModel from './HomeModel';
 
 class HomeController extends Component {
     constructor(props){
         super(props);
+
+        this.state = {
+            count: 0
+        }
+
+        const homeModel = new HomeModel();
+        homeModel.getSomeInfo();
+
     }
-    
+
+    componentDidMount(){
+        console.log("Mount...")
+
+        this.interval = setInterval(() =>{
+        
+            if(this.state.status === "Running"){
+                this.setState({
+                    count: this.state.count +1,
+                });
+            }
+        }, 1000)
+    }
+
+    start = () =>{
+        this.setState({
+            status: "Running"
+        })
+    }
+
+    paused = () =>{
+        this.setState({
+            status: "Paused"
+        })
+    }
+
+    stop = () =>{
+        this.setState({
+            status: "Ending"
+        })
+    }
+
+    clear = () =>{
+        this.setState({
+            count: 0
+        })
+    }
+
     render() {
         return (
-            <HomeView/>
+            <HomeView
+            count={this.state.count}
+            start={this.start}
+            paused={this.paused}
+            stop={this.stop}
+            clear={this.clear}
+            />
         );
     }
 }
--- module5/fiap-shift-exercise03/src/Screen/Homes/HomeView.js
+++ module5/fiap-shift-exercise03/src/Screen/Homes/HomeView.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Home.css';
 
-const HomeView = ({ count }) => {
+const HomeView = ({count}) => {
 
     return (
         <div>
-            <p>CONTADOR {count}</p>
+            <p>COUNT: {count}</p>
+            <button>Start</button>
+            <button>Pause</button>
+            <button>Stop</button>
+            <button>Clear</button>
         </div>
     );
 };
