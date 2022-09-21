f87df00553db629bd73c3ac3eaa11f582bfd2f1f
belluzzojr <belluzzojrb@gmail.com>
2022-09-21 13:56:40 -0300

    React Framework

---
+++ module5/fiap-shift-exercise09/src/Screen/Home/HomeController.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import HomeView from './HomeView'
+
+
+export default function HomeController () {
+    
+    return (
+      <HomeView/>
+    )
+}
---
+++ module5/fiap-shift-exercise09/src/Screen/Home/HomeView.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function HomeView (){
+
+    return (
+      <button onClick={() => {
+        alert("System in Development!!")
+      }}>
+        CLIQUE AQUI
+      </button>
+    )
+}
